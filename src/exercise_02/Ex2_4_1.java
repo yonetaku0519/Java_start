37c2f617c8d1f42b430c2b66a30b759d26941a83
yonezawatakuya <yonezawatakuya@yonezawuyanombp.elecom>
2021-10-06 13:23:10 +0900

    第二章修了

---
+++ src/exercise_02/Ex2_4_1.java
@@ -0,0 +1,10 @@
+package exercise_02;
+
+public class Ex2_4_1 {
+	public static void main(String[] args) {
+		System.out.print(1024);;
+		System.out.println("こんにちは");;
+		System.out.println("さようなら");
+	}
+
+}
---
+++ src/exercise_02/Pass_test.java
@@ -0,0 +1,12 @@
+package exercise_02;
+
+public class Pass_test {
+	public static void main(String[] args) {
+		System.out.print(1000);
+		System.out.println("田中宏");
+		System.out.print(1010);
+		System.out.println("谷川太郎");
+		System.out.print(1020);
+		System.out.println("鈴木誠");
+	}
+}
