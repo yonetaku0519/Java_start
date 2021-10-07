10da4a0fc7caffb059dcbe8f57a4fb5e7d550326
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-07 15:06:27 +0900

    Create Pass4_4.java
    第四章修了

---
+++ src/exercise_04/Pass4_4.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Pass4_4 {
+	public static void main(String[] args) {
+		double area;
+		double radius = 12.5;
+		area = radius * radius * 3.14;
+		System.out.println("面積＝" +area);
+	}
+
+}
