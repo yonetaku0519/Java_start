5197ea22a41f64eefa030a5dfd1e42e46eb9bc65
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-08 17:15:40 +0900

    Create Pass6_3.java

---
+++ src/exercise_06/Pass6_3.java
@@ -0,0 +1,12 @@
+package exercise;
+
+public class Pass6_3 {
+
+	public static void main(String[] args) {
+		double a = Math.random();
+		int p = (int)(6 * a + 1);
+		System.out.println("サイコロの目数" + p);
+
+	}
+
+}
