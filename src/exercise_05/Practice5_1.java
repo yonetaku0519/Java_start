0624ae9bd79a817b44f973ade67e21a36203f6ba
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-08 10:12:12 +0900

    Create Practice5_1.java

---
+++ src/exercise_05/Practice5_1.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Practice5_1 {
+	public static void main(String[] args) {
+		int n = 1, ans;
+		System.out.println(++n);
+		System.out.println(n++);
+		ans = 1 + n++;
+		System.out.println(ans);
+		System.out.println(n);
+	}
+
+}
