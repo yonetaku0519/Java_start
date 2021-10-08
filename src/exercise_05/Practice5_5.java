f775241754e6b7f60d04161b6c033c85b4f619ff
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-08 10:44:19 +0900

    Create Practice5_5.java

---
+++ src/exercise_05/Practice5_5.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class Practice5_5 {
+	public static void main(String[] args) {
+		int a, b, c;
+		a = (int)27.56;
+		b = (int)128L;
+		c = (int)(357.33 % 6);
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+		System.out.println("c=" + c);
+	}
+	åç
+}
