fde01ff1b1b08834e4d56b263782953b28480716
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-09 17:25:42 +0900

    Create Pass7_1.java

---
+++ src/exercise_07/Pass7_1.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Pass7_1 {
+	public static void main(String[] args) {
+		String[] str = {"月曜日","火曜日","水曜日","木曜日","金曜日","土曜日","日曜日"};
+		for(int i = 0; i < str.length; i++) {
+			System.out.print(str[i] + "\t");
+		}
+	}
+}
