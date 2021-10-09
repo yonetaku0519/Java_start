c1b5c5cbe913dade7133bbe3e68144d5bff69b6d
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-09 17:15:52 +0900

    Create Pracrice7_5.java

---
+++ src/exercise_07/Pracrice7_5.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Practice7_5 {
+	public static void main(String[] args) {
+		int[] n1 = {12, 11, 32};
+		int[] n2 = {26, 14, 12};
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.println(n1[i] + "+" + n2[i] + "=" + (n1[i] + n2[i]));
+		}
+	}
+
+}
