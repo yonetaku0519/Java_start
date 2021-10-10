4e50fe2fbc29f8831513ffa7c2b4530cc514dc29
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-10 13:59:14 +0900

    Create Practice8_9_2.java

---
+++ src/exercise_08/Practice8_9_2.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Practice8_9_2 {
+	public static void main(String[] args) {
+		double[] data = {12.5, 2.88, 3.6, 0.123, 1.22, 3.334};
+		for(double d : data) {
+			System.out.print(d + "\t");
+		}
+	}
+}
