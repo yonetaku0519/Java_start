be016e214eb3a602b35c18ba8e2910692701cf8e
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-10 13:54:39 +0900

    Create Practice8_9_1.java

---
+++ src/exercise_08/Practice8_9_1.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Practie8_9_1 {
+	public static void main(String[] args) {
+		int[] number = {10, 13, 34, 29, 17};
+		for(int i: number) {
+			System.out.print(i + "\t");
+		}
+	}
+}
