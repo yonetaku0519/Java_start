6e70033bc5df5b21c3fbe0cd931774aa95923deb
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-19 19:53:44 +0900

    Create Practice15_3.java

---
+++ src/exercise_15/Practice15_3.java
@@ -0,0 +1,10 @@
+package exercise;
+
+public class Practice15_3 {
+	public static void main(String[] args) {
+		int[] n = {12, 11, 25, 7, 18, 3};
+		int max = MyTools.max(n);
+		System.out.println("最大値=" + max);
+	}
+
+}
