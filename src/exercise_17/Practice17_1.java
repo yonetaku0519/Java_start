87fc7113fadbf303015ddc136b603de658b17181
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-21 10:18:14 +0900

    Create Practice17_1.java

---
+++ src/exercise_17/Practice17_1.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import sample.Sales;
+
+public class Practice17_1 {
+	public static void main(String[] args) {
+		Sales[] sales = {
+				new Sales("SY-200", "冷蔵庫", 50000, 20, true),
+				new Sales("TB-100", "洗濯機", 30000, 15, true),
+				new Sales("AX-551", "テレビ", 15000, 32, false),
+				new Sales("XS-112", "掃除機", 22000, 23, false)
+		};
+		
+		for(Sales s : sales) {
+			if(s.isHightSales()) {
+				System.out.print(s.getCode() + " ");
+				System.out.print(s.getName() + " ");
+				System.out.print(s.getPrice() + " ");
+				System.out.print(s.getQuantity() + " ");
+				System.out.println(s.isStock() + " ");
+			}
+		}
+	}
+
+}
