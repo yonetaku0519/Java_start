02fa478a6537571b3f5ce364117da6377a045df2
yonetaku0519 <83192846+yonetaku0519@users.noreply.github.com>
2021-10-25 15:10:55 +0900

    Create Shape.java

---
+++ src/exercise_18/Shape.java
@@ -0,0 +1,19 @@
+package exercise;
+
+public class Shape {
+	private String color;
+
+	public Shape(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+
+}
